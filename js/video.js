6e2a12d2f0d1ad4f0d90c3a07a34ddfd5a92ce47
vinga19 <vitorhugo_rib@hotmail.com>
2019-05-05 12:17:12 -0300

    Criando Projeto Portfólio

---
+++ js/video.js
@@ -0,0 +1,38 @@
+
+/*
+var test = document.getElementById("logo")
+
+var logo1 = "logo_rcs3.png"
+var logo2 = "logo_rcs3_white.png"
+function trocar(){
+	src_logo = document.getElementById("logo").src.split("/");
+	len_logo = src_logo.length;
+	if(src_logo[len_logo-1]==logo1)
+		{
+			test.src = "./img/"+logo2;
+			console.log("Logo 2");
+		}else {
+		test.src = "./img/"+logo1;
+		console.log("Logo 1");
+	return test;
+
+	}
+}
+
+*/
+var mostra = true;
+
+function trocar(){
+	mostra = !mostra;
+	console.log(mostra)
+	if(mostra == false){
+		document.getElementById('logo').style.display = "none";
+		document.getElementById('logo2').style.display = "block";
+	}else if(mostra == true){
+		document.getElementById('logo').style.display = "block";
+		document.getElementById('logo2').style.display = "none";
+	}
+	
+}
+
+
